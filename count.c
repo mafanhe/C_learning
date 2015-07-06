7f040c16882c622e374e56d0fd0a44f955926faa
马凡贺 <827149941@qq.com>
2015-07-06 15:01:41 +0800

    Create count.c

---
+++ count.c
@@ -0,0 +1,20 @@
+//shuzu ji shu qi
+#include <stdio.h>
+int main(){
+  const int number=10;
+  int x;
+  int count[number];
+  int i;
+  for(i=0;i<number;i++){
+    count[i]=0;
+  }
+  scanf("%d",&x );
+  while(x!=-1){
+    for(i=0;i<number;i++){
+      if(x>=0&&x<=9) count[x]++;
+      scanf("%d",&x );
+    }
+  }
+  for(i=0;i<number;i++)printf("%d:%d\n",i,count[i] );
+  return 0;
+}
